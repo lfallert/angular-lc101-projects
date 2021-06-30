774c48b019666297cf9beb7159ca1d7029872426
LFallert <lfallert13@yahoo.com>
2021-06-30 16:34:34 -0500

    finished exercise

--- lesson2/exercises/src/app/candidates/candidates.component.ts
+++ lesson2/exercises/src/app/candidates/candidates.component.ts
@@ -25,6 +25,12 @@ export class CandidatesComponent implements OnInit {
 
   // Code the addToCrew function here:
 
+  addToCrew(person: object) {
+  if (!this.crew.includes(person)) {
+    this.crew.push(person);
+    }
+  }
+
 
   // BONUS: Code the changeMissionName function here:
 
